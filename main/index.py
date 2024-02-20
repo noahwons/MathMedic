0a4838056287e1f1cef1f7b2476daef276697353
Noah <wons123@iastate.edu>
2024-02-20 17:16:44 -0600

    Add basic index method

--- main/index.py
+++ main/index.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__) 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-@app.route('/')    
+@app.route('/', methods=["GET", "POST"])    
 def index():
-    return "Hello, World!"
+    if request.method == "GET":
+        # do sum shit
+        pass
+    else:
+        return render_template("index.html")
+
     
 if __main__ == "__name__":
     main()
