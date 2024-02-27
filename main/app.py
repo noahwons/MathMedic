0488ebca915534e37e684d5b5b47a5fa77f26f9d
Noah <wons123@iastate.edu>
2024-02-27 17:37:21 -0600

    Add functional Calculator, TODO: Implement function to parse user information, Implement session_id, Implement SQLite database, etc.

--- main/app.py
+++ main/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from sympy import *
+from sympy.abc import x
 
 app = Flask(__name__) 
 app.config["SESSION_PERMANENT"] = False
@@ -26,10 +28,19 @@ def calculate():
         return render_template("calculate.html")
     else:
         expression = request.form.get("expression")
-        for char in expression:
-            if char == '+':
-                result =  int(expression[0]) + int(expression[2])
-        return render_template("calculate.html", expression=result)
+        return render_template("calculate.html", expression=sympify(expression, evaluate=True))
+    
+    
+@app.route('/simplify', methods=["GET", "POST"])        
+def simplify():
+    #TODO: Does not yet work as intended
+    if request.method == "GET":
+        return render_template("simplify.html")
+    else:
+        x = symbols('x')
+        expression = simplify(sympify(request.form.get("expression")))
+        
+        return render_template("simplify.html", expression=expression)
     
     
 if __name__ == "__main__":
