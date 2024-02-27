c694189495d142cf5cbd67fa57f7a70d153de64f
Noah <wons123@iastate.edu>
2024-02-27 17:08:32 -0600

    Add very broken addition logic and new page for calculator

--- main/app.py
+++ main/app.py
@@ -9,10 +9,28 @@ Session(app)
 @app.route('/', methods=["GET", "POST"])    
 def index():
     if request.method == "GET":
+        return render_template("index.html")
+    
+    
+@app.route('/login', methods=["GET", "POST"])    
+def login():
+    if request.method == "GET":
         return render_template("login.html")
     else:
         return render_template("login.html")
 
+
+@app.route('/calculate', methods=["GET", "POST"])        
+def calculate():
+    if request.method == "GET":
+        return render_template("calculate.html")
+    else:
+        expression = request.form.get("expression")
+        for char in expression:
+            if char == '+':
+                result =  int(expression[0]) + int(expression[2])
+        return render_template("calculate.html", expression=result)
+    
     
 if __name__ == "__main__":
     main()
